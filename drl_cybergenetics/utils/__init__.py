4704ee15a0e54af89e3972e5073e105d76780bd4
qbadolle <qbadolle@ethz.ch>
2022-06-01 23:04:10 +0200

    update architecture

--- drl_cybergenetics/utils/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-from .episodic_logger import EpisodicLogger
-from .bs_scheduler import *
-from .er_scheduler import *
-from .plotter import *
-
-__all__ = ('EpisodicLogger')
