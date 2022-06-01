fe3f476d4e4c3dbea891a958e1b3d690c7ad8154
qbadolle <93556598+qbadolle@users.noreply.github.com>
2022-06-01 23:07:08 +0200

    Update __init__.py

--- drl_cybergenetics/__init__.py
+++ drl_cybergenetics/__init__.py
@@ -1,11 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from . import interactor
-from . import env
-from . import agent
-from . import nn
-from . import data
-from . import utils
+from . import envs
+from . import agents
 
-__all__ = ('interactor', 'env', 'agent', 'nn', 'data', 'utils')
+__all__ = ('envs', 'agents', 'nn')
